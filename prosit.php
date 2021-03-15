0bf5e84aeac36708e27e2a2acb4342de8920f00e
Mattéo COFFIN <matteo.coffin@viacesi.fr>
2021-03-15 10:35:20 +0100

    add elements

--- prosit.php
+++ prosit.php
@@ -11,23 +11,29 @@ session_start();
     <body>
         <h1>Ma page web</h1>
         <?php
-        
+        // permet de connaitre le choix de l'utilisateur (précédent ou suivant)
             if(isset($_GET['previous'])){
-                $_SESSION['caractere'] -= 512; 
+                $_SESSION['caractere'] -= 512;
+                $_SESSION['PAGE']--;
+            }else{
+                $_SESSION['PAGE']++;
             }
-            $first = $_SESSION['caractere'];
+        //création d'une table avec les smileys
             echo '<table>';
             for($i = 0;$i<16;$i++){
                 echo '<tr>';
+                //génération de caractères
                 for($j = 0;$j<16;$j++){
                     $_SESSION['caractere']++;
                     echo '<td>&#'.$_SESSION['caractere'].'</td>';
                 }
                 echo '</tr>';
-                $_SESSION['precedent']= $first;
-            }
+               }
         ?>
-        <a href="prosit.php?previous=true">precedant</a>
-        <a href="prosit.php?">suivant</a>
+        <a href="prosit.php?previous=true">precedent</a>
+        <?php echo '-----'.$_SESSION['PAGE'].'-----' ?>
+        <a href="prosit.php">suivant</a>
+        <br>
+        <a href="mentionlegal.php">Mention légale</a>
     </body>
 </html>
